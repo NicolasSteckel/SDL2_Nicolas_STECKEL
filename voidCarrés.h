f78879bb28fa6bd72e61df0f63fb250c78a05f16
NicolasSTECKEL <n.steckel@ludus-academie.com>
2020-03-16 14:36:39 +0100

    no message

---
+++ voidCarrés.h
@@ -0,0 +1,11 @@
+#ifndef VOIDCARRES_H_INCLUDED
+#define VOIDCARRES_H_INCLUDED
+#include <stdio.h>
+#include "SDL2/SDL.h"
+
+int carreVide(SDL_Renderer *pRenderer, SDL_Rect*rect);
+
+int carrePlein(SDL_Renderer *pRenderer, SDL_Rect*rectP);
+
+
+#endif // VOIDCARRÈS_H_INCLUDED
